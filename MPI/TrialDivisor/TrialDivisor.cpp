16d50c4973aaa3d03a3da8e46d63c3e9607e5f77
cbarbes1 <cbarbes02@gmail.com>
2024-04-11 00:19:47 -0400

    added some stuff for mpi

--- MPI/TrialDivisor/TrialDivisor.cpp
+++ MPI/TrialDivisor/TrialDivisor.cpp
@@ -1,25 +1,30 @@
 #include "mpi.h"
 #include <iostream>
 #include <cmath>
+#include <vector>
 
-int factor_range(int, int, int); // function to factor numbers in a given range
+//vector<int> factor_range(int, int, int); // function to factor numbers in a given range
 
 int main(int argc, char** argv)
 {
-    // if(argc != 2){
-    //     std::cerr<<"Invalid arguments!"<<std::endl;
-    // }
-    //int trial_count = atoi(argv[1]);
-    std::cout<<"Starting Trial Divisor Factoring Algorithm"<<std::endl;
-    MPI_Init(NULL, NULL);
-
     int world_size, world_rank;
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    //int number_of_test = trial_count / world_size;
+    int number = static_cast<int>(sqrt(atoi(argv[1])));
+    int range_size = number / world_size;
+    int start = world_rank + (range_size*world_rank) + 1;
+    int end = range_size + (world_rank*range_size);
 
-    printf("%d\n", world_rank);
+    printf("Process %d computing range[ %d, %d ]\n", world_rank, start, end);
+
+    for(int i = start; i<=end; i++){
+        if(number % i == 0){
+            printf("Process %d found factor: %d\n", world_rank, i);
+        }
+        //sleep(6);
+    }
     
     MPI_Finalize();
 }
@@ -28,7 +33,13 @@ int main(int argc, char** argv)
 *   Given a number and a range to factor this function will find a factor of a number using trial division
 *   Parameters: the number, the start of the 
 */
-int factor_range(int, int, int){
+// vector<int> factor_range(int start, int end, int number){
+//     vector<int>
+//     for(int i = start; i<=end; i++){
+//         if(number % i == 0){
+            
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ MPI/TrialDivisor/testRun.cpp
@@ -0,0 +1,30 @@
+#include "mpi.h"
+#include <iostream>
+#include <cmath>
+
+void err_sys(MPI_Comm comm, const char *err){
+    printf("%s\n", err);
+    MPI_Abort(comm, 0);
+}
+
+int main(int argc, char** argv)
+{
+    int world_size, world_rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int number;
+
+    if(world_rank == 0){
+        printf("Hello my children I am your father at rank %d\n", world_rank );
+        if(argc < 2){
+            err_sys(MPI_COMM_WORLD,"Too few Arguments");
+        }
+        number = atoi(argv[1]);
+    }
+    MPI_Bcast(&number, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d recieved data : %d\n", world_rank, number);
+    MPI_Finalize();
+    return 0;
+}
